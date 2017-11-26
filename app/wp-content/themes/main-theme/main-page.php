6c66dccadf0dade01758287f107183ccdf7eec4e
Mark Aborchie <mark.aborchie@gmail.com>
2017-11-26 15:48:19 +0300

    one section complete

--- app/wp-content/themes/main-theme/main-page.php
+++ app/wp-content/themes/main-theme/main-page.php
@@ -10,7 +10,56 @@ get_header(); ?>
 		<div class="row">
 			<div class="container">
 				<div class="row">
-					<div class="col-12">Our Services</div>
+					<div class="col-12 align-c h2-main">Our Services</div>
+					<div class="col-12 desc-text align-c">Our company is based on a family tradition of superior floor covering and ceramic tile installation services. My family began a store much like today’s decorator stores back in the early 60’s.   </div>
+					<div class="col-12">
+						<div class="row">
+							<div class="col-6">
+								<div class="col-12 service-wrapper">
+									<div class="row height-100">
+										<div class="col-12 service-value align-self-center">
+											<div class="row height-100">
+												<div class="col-12 align-self-center align-c service-text">Lorem ipsum dolor sit amet.</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="col-12 service-wrapper">
+									<div class="row height-100">
+										<div class="col-12 service-value align-self-center">
+											<div class="row height-100">
+												<div class="col-12 align-self-center align-c service-text">Lorem ipsum dolor sit amet.</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="col-12 service-wrapper">
+									<div class="row height-100">
+										<div class="col-12 service-value align-self-center">
+											<div class="row height-100">
+												<div class="col-12 align-self-center align-c service-text">Lorem ipsum dolor sit amet.</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="col-12 service-wrapper">
+									<div class="row height-100">
+										<div class="col-12 service-value align-self-center">
+											<div class="row height-100">
+												<div class="col-12 align-self-center align-c service-text">Lorem ipsum dolor sit amet.</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -18,5 +67,21 @@ get_header(); ?>
 </section>
 
 <!-- Our Services (finish) -->
+
+<!-- Latest Projects (start) -->
+
+<section id="sect-latest-projects">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 align-c h2-main">Look Our Latest Projects</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Latest Projects (finish) -->
 
 <?php get_footer(); ?>
